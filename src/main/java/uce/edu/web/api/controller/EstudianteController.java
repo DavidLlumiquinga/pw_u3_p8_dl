d5f90b1202c340156f1dc2babeb89961bde44a06
DavidLlumiquinga <mdllumiquingat@uce.edu.ec>
2025-07-02 19:48:12 -0500

    TallerTarea

--- src/main/java/uce/edu/web/api/controller/EstudianteController.java
+++ src/main/java/uce/edu/web/api/controller/EstudianteController.java
@@ -1,11 +1,10 @@
 package uce.edu.web.api.controller;
 
-import java.util.List;
-
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 
 import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.PATCH;
@@ -13,7 +12,10 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import uce.edu.web.api.repository.modelo.Estudiante;
 import uce.edu.web.api.service.IEstudianteService;
 
@@ -25,20 +27,25 @@ public class EstudianteController {
 
     @GET
     @Path("/{id}")
-    public Estudiante consultarPorId(@PathParam("id")Integer id) {
-        return this.estudianteService.buscarPorId(id); 
+    @Produces(MediaType.APPLICATION_XML)
+    public Response consultarPorId(@PathParam("id")Integer id) {
+        return Response.status(227).entity(this.estudianteService.buscarPorId(id)).build(); 
     }
 
     //?genero=M&provincia=Pichincha
     @GET
     @Path("")
-    public List<Estudiante> consultarTodos(@QueryParam("genero") String genero, 
-                                           @QueryParam("provincia") String provincia) {
-        return this.estudianteService.buscarTodos(genero);
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response consultarTodos(@QueryParam("genero") String genero, 
+                        @QueryParam("provincia") String provincia) {
+        System.out.println(provincia);
+        return Response.status(Response.Status.OK).entity(this.estudianteService.buscarTodos(genero)).build();
+            
     }
 
     @POST
     @Path("")
+    @Consumes(MediaType.APPLICATION_JSON)
     @Operation(summary = "Guardar estudiantes",description = "Esta capacidad permite guardar un estudiante")
     //Puede tener o no tener el @RequestBody
     public void guardar(@RequestBody Estudiante estudiante) {
@@ -48,6 +55,7 @@ public class EstudianteController {
 
     @PUT
     @Path("/{id}")
+        @Consumes(MediaType.APPLICATION_JSON)
     public void actualizar(@PathParam("id") Integer id, @RequestBody Estudiante estudiante) {
         estudiante.setId(id);
         this.estudianteService.actualizarPorId(estudiante);
@@ -55,6 +63,7 @@ public class EstudianteController {
 
     @PATCH
     @Path("/{id}")
+        @Consumes(MediaType.APPLICATION_JSON)
     public void actualizarParcialPorId(@PathParam("id") Integer id, @RequestBody Estudiante estudiante) {
         estudiante.setId(id);
         Estudiante e=this.estudianteService.buscarPorId(id);
@@ -73,6 +82,7 @@ public class EstudianteController {
 
     @DELETE
     @Path("/{id}")
+        @Consumes(MediaType.APPLICATION_JSON)
     public void borrarPorId(@PathParam("id") Integer id) {
         this.estudianteService.borrarPorId(id);
     }
